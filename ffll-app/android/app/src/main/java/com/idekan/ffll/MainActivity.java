43102ef9102d13f58d476bd0c58a33acf0ac8da9
JeremiahGironGD <jeremiahgironyt@gmail.com>
2026-06-05 22:06:48 -0600

    Changes to be implemented

--- ffll-app/android/app/src/main/java/com/idekan/ffll/MainActivity.java
+++ ffll-app/android/app/src/main/java/com/idekan/ffll/MainActivity.java
@@ -1,12 +1,11 @@
 package com.idekan.ffll;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.content.Intent;
-import android.app.AlertDialog;
-import android.widget.Button;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -17,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Calendar;
 
 public class MainActivity extends BridgeActivity {
     private static final String GITHUB_API_URL = "https://api.github.com/repos/JeremiahGironGD/FFLL/releases/latest";
@@ -32,7 +30,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         checkForUpdates();
     }
