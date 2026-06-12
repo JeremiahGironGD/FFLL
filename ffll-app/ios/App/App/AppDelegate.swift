91b2d1409a2e365cd853f12e64c6955e2f22ed77
JeremiahGironGD <jeremiahgironyt@gmail.com>
2026-06-12 17:10:40 -0600

    iOS patch Fix #1

--- ffll-app/ios/App/App/AppDelegate.swift
+++ ffll-app/ios/App/App/AppDelegate.swift
@@ -6,36 +6,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return ApplicationDelegateProxy.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    func applicationWillResignActive(_ application: UIApplication) {}
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+    func applicationDidEnterBackground(_ application: UIApplication) {}
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+    func applicationWillEnterForeground(_ application: UIApplication) {}
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         self.checkForUpdates()
     }
 
     func checkForUpdates() {
         let lastCheckKey = "ffll-last-update-check-ios"
-        // Dynamically get the version from the project's Info.plist
         let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0"
         let updateCheckInterval: TimeInterval = 24 * 60 * 60 // 24 hours
         
-        // Check if we've already checked recently
         if let lastCheck = UserDefaults.standard.object(forKey: lastCheckKey) as? TimeInterval {
             let timeSinceCheck = Date().timeIntervalSince1970 - lastCheck
             if timeSinceCheck < updateCheckInterval {
@@ -43,7 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         
-        // Check GitHub API for latest release
         let urlString = "https://api.github.com/repos/JeremiahGironGD/FFLL/releases/latest"
         guard let url = URL(string: urlString) else { return }
         
@@ -51,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         request.setValue("FFLL-App", forHTTPHeaderField: "User-Agent")
         
         URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            // Update check timestamp immediately to prevent spamming
             UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: lastCheckKey)
             
             guard let data = data, error == nil else { return }
@@ -74,12 +61,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func isNewerVersion(_ latestVersion: String, currentVersion: String) -> Bool {
-        // Use ComparisonResult explicitly to avoid ambiguity and improve readability
-        return latestVersion.compare(currentVersion, options: .numeric) == ComparisonResult.orderedDescending
+        return latestVersion.compare(currentVersion, options: .numeric) == .orderedDescending
     }
     
     func showUpdateNotification(latestVersion: String, json: [String: Any]) {
-        // Get the app's key window
         guard let keyWindow = UIApplication.shared.connectedScenes
             .compactMap({ $0 as? UIWindowScene })
             .flatMap({ $0.windows })
@@ -91,7 +76,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             preferredStyle: .alert
         )
         
-        // Find download URL
         var downloadUrl = "https://github.com/JeremiahGironGD/FFLL/releases/latest"
         if let assets = json["assets"] as? [[String: Any]] {
             for asset in assets {
@@ -103,17 +87,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         
-        // Update action
         alertController.addAction(UIAlertAction(title: "Update Now", style: .default) { _ in
             if let url = URL(string: downloadUrl) {
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
             }
         })
         
-        // Later action
         alertController.addAction(UIAlertAction(title: "Later", style: .cancel))
         
-        // Find the top-most view controller to present the alert
         var topController = keyWindow.rootViewController
         while let presentedViewController = topController?.presentedViewController {
             topController = presentedViewController
@@ -124,21 +105,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    func applicationWillTerminate(_ application: UIApplication) {}
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
-
 }
