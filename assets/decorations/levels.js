3b0322f6764fff7ebda6df58e7b17efc0bf36e38
JeremiahGironGD <jeremiahgironyt@gmail.com>
2026-08-04 02:41:25 -0600

    doing random stuff to make my life easier down the road :D

---
+++ assets/decorations/levels.js
@@ -0,0 +1,72 @@
+export const levels = [
+{
+    rank: 1,
+    title: "Not decided",
+    points: 100,
+    tier: "gold",
+    url: "BoTF.html"
+},
+{
+    rank: 2,
+    title: "Not decided",
+    points: 80,
+    tier: "silver",
+    url: "thatonepurplelevel.html"
+},
+{
+    rank: 3,
+    title: "Not decided",
+    points: 75,
+    tier: "bronze",
+    url: "idrinkbatteryacid.html"
+},
+{
+    rank: 4,
+    title: "Not decided",
+    points: 0,
+    tier: "dark",
+    url: "foreverandeverpreview.html"
+},
+{
+    rank: 5,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",
+    url: "wave17remake.html"
+},
+{
+    rank: 6,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",
+    url: "jacecircles.html"
+},
+{
+    rank: 7,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",
+    url: "sonicblaster.html"
+},
+{
+    rank: 8,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",    
+    url: "jacechallenge.html"    
+},
+{
+    rank: 9,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",
+    url: "javascript:void()"
+},
+{
+    rank: 10,
+    title: "Not decided",
+    points: 50,
+    tier: "dark",
+    url: "javascript:void()"
+}
+];
